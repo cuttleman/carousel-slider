8ce4753de4a2bfedbef28488a39c53fa837454d8
ddamjengi1020 <silluat11@gmail.com>
2020-05-20 15:00:02 +0900

    git revert

--- README.md
+++ README.md
@@ -7,7 +7,3 @@ create carousel slider sample (in place, slide)
 [] Flicker in place
 
 [x] Srrrr... Slide (not buttonm, not dot)
-
-<video width="320" height="240" controls>
-  <source src="nextSlide.mp4" type="video/mp4">
-</video>
--- main.js
+++ main.js
@@ -1,34 +1,40 @@
-const PREV = "prev";
-const ACTIVE = "active";
-const NEXT = "next";
-
 const first = document.querySelector(".slide:nth-child(1)");
 
-const slideEvent = () => {
+const nextSlideEvent = (duration) => {
   // 매 함수 호출시마다 브라우저 width 길이를 업뎃함 -> 애니메이션 변수로 사용하기떄문
   const WINDOW = window.innerWidth;
+  const PREV = "prev";
+  const ACTIVE = "active";
+  const NEXT = "next";
   const activeEl = document.getElementById(ACTIVE);
   const nextEl = document.getElementById(NEXT);
   const prevEl = document.getElementById(PREV);
-  if (activeEl) {
+  if (!activeEl) {
+    first.id = ACTIVE;
+    first.style.zIndex = 1;
+    first.nextElementSibling.id = NEXT;
+  } else {
     if (prevEl) {
       prevEl.removeAttribute("id");
     }
     activeEl.id = PREV;
     nextEl.id = ACTIVE;
+    first.removeAttribute("style");
     activeEl.animate(
       // 실행 시점에선 nextEl은 Prev 상태의 Element를 가리킴
       {
         transform: [`translateX(0)`, `translateX(-${WINDOW}px)`],
+        zIndex: [1, 1],
       },
-      1000
+      { duration: duration ? duration : 0, fill: "forwards" }
     );
     nextEl.animate(
       // 실행 시점에선 nextEl은 현재 Active 상태의 Element를 가리킴
       {
         transform: [`translateX(${WINDOW}px)`, `translateX(0)`],
+        zIndex: [1, 2],
       },
-      1000
+      { duration: duration ? duration : 0, fill: "forwards" }
     );
     if (!nextEl.nextElementSibling) {
       first.id = NEXT;
@@ -38,11 +44,5 @@ const slideEvent = () => {
   }
 };
 
-const init = () => {
-  // absolute해준 slide들이 오름차순으로 정렬되어있기때문에 onload되었을때 처음 Element를 맨 위로 올려줌
-  first.id = ACTIVE;
-  first.nextElementSibling.id = NEXT;
-  setInterval(() => slideEvent(), 3500);
-};
-
-init();
+nextSlideEvent();
+setInterval(() => nextSlideEvent(1000), 3500);
